06159dc617e419ef991895862cd0cc0aa2ed2cb6
Joel Rivera <joel.rivera7995@gmail.com>
2018-11-06 00:01:17 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -177,12 +177,12 @@ end
 
 def big_shoe_rebounds
   shoe_size = 0
-  rebound = 0
+  rebounds = 0
   game_hash.each do |location, team_data|
     team_data[:players].each do |player_name, value|
       if value[:shoe] > shoe_size
         shoe_size = value[:shoe]
-        rebound = value[:rebounds]
+        rebounds = value[:rebounds]
       end
     end
   end
